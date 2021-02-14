8efe3ba14f0c8fa0678c5aa4222446d71dcc5899
Florian Demmer <fdemmer@gmail.com>
2021-02-14 23:03:57 +0100

    Add LOGGING settings

--- play/settings.py
+++ play/settings.py
@@ -118,3 +118,23 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Logging
+# https://docs.djangoproject.com/en/2.2/topics/logging/
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+        }
+    }
+}
